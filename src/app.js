b09489e5e7854da957b806f2ddba6d6e744dbe6c
yeongtaek, Hong <mozodev@users.noreply.github.com>
2020-09-07 23:26:25 +0900

    고딕1 폰트와 프로젝트 커버 이미지를 추가합니다.

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ $(() => {
     $('html').addClass('dom-loaded')
     $('<span class="text-muted">Appended with Cash</span>').appendTo($('footer .container'))
 
-    var alertList = document.querySelectorAll('.alert')
-    alertList.forEach(function (alert) {
+    var alertList = $('.alert')
+    alertList.each( (index, alert) => {
         new Alert(alert)
     })
 })
