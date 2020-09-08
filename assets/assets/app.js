46fd612855c9f94c6cb45e3420c40527d332580c
yeongtaek, Hong <mozodev@users.noreply.github.com>
2020-09-08 14:47:38 +0900

    코드를 정리합니다.

--- assets/assets/app.js
+++ assets/assets/app.js
@@ -9262,7 +9262,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49702" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55263" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
